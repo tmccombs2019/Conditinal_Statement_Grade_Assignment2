3edaa20ff73dc37982470161cf96c1e97a238082
tmccombs2019 <tracey.mccombs@verizon.net>
2019-01-21 00:50:27 -0500

    Add project files.

---
+++ Conditinal_Statement_Grade_Assignment2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Conditinal_Statement_Grade_Assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
